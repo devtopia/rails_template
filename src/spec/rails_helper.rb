1e4c811225248a4c29b111ee95d52ebd9d61bd65
park-jh <park-jh@gameon.co.jp>
2015-09-14 14:57:20 +0900

    To comment out the reset_email

--- src/spec/rails_helper.rb
+++ src/spec/rails_helper.rb
@@ -47,7 +47,7 @@ RSpec.configure do |config|
   config.include EmailSpec::Helpers
   config.include EmailSpec::Matchers
 
-  config.before(:each) { reset_email }
+  # config.before(:each) { reset_email }
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
